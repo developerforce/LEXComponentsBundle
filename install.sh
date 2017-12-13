46fed7f73266c59111f0ec7df418310ea75be65c
Dave Carroll <djcarroll@gmail.com>
2017-12-13 09:54:50 -0800

    update sratchdef/install script

--- install.sh
+++ install.sh
@@ -4,5 +4,7 @@ printf "Enter a name for your scratch org: "
 read orgname
 echo "Creating scratch org named "$orgname
 sfdx force:org:create -a $orgname -f config/project-scratch-def.json
+echo "Pushing LEX Components source to $orgname..."
 sfdx force:source:push -u $orgname
+echo "Opening $orgname in the browser..."
 sfdx force:org:open -u $orgname
