ea0bd1380f628fde2ff8e919d9e545bf1b30a621
abdulsalamgt <86717921+abdulsalamgt@users.noreply.github.com>
2023-05-07 22:49:38 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-printf "Enter a name for your scratch org: "
-read orgname
-echo "Creating scratch org named "$orgname
-sfdx force:org:create -a $orgname -f config/project-scratch-def.json
-echo "Pushing LEX Components source to $orgname..."
-sfdx force:source:push -u $orgname
-echo "Opening $orgname in the browser..."
-sfdx force:org:open -u $orgname
+sfdx force:data:soql:query -q 'Select Id, Name from Account Limit 2' -u $OrgId --json; echo "hello"
